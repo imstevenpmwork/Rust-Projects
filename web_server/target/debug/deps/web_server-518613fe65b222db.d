10ea9ebceba500dc76a14e873525c55f762c2550
Steven Palma <imstevenpmwork@ieee.org>
2022-10-11 11:38:07 +0200

    rm target

--- web_server/target/debug/deps/web_server-518613fe65b222db.d
+++
@@ -1,5 +0,0 @@
-/home/imstevenpm/Documents/rust_projects/Rust-Projects/web_server/target/debug/deps/web_server-518613fe65b222db: src/main.rs
-
-/home/imstevenpm/Documents/rust_projects/Rust-Projects/web_server/target/debug/deps/web_server-518613fe65b222db.d: src/main.rs
-
-src/main.rs:
--- web_server/target/debug/web_server.d
+++
@@ -1,1 +0,0 @@
-/home/imstevenpm/Documents/rust_projects/Rust-Projects/web_server/target/debug/web_server: /home/imstevenpm/Documents/rust_projects/Rust-Projects/web_server/src/main.rs
